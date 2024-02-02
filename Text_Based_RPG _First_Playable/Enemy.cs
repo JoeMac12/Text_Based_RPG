dd6d09b27b353b5076abfa08020b6f3041a2e762
Arman <144450534+JoeMac12@users.noreply.github.com>
2024-02-02 00:29:07 -0400

    readd player and enemy classes with fixes

---
+++ Text_Based_RPG _First_Playable/Enemy.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Enemy
+{
+    public (int x, int y) Position { get; private set; }
+    private HealthSystem healthSystem;
+    private Map map;
+    private Random random;
+
+    public Enemy(Map map, int initialHealth, int startX, int startY)
+    {
+        this.map = map;
+        Position = (startX, startY);
+        healthSystem = new HealthSystem(initialHealth);
+        random = new Random();
+    }
+
+    public void MoveRandomly()
+    {
+        int direction = random.Next(4);
+        int x = 0, y = 0;
+
+        switch (direction)
+        {
+            case 0: y = -1; break;
+            case 1: y = 1; break;
+            case 2: x = -1; break;
+            case 3: x = 1; break;
+        }
+
+        int newX = Position.x + x;
+        int newY = Position.y + y;
+
+        if (map.WithinBounds(newX, newY) && CanMove(newX, newY))
+        {
+            Position = (newX, newY);
+        }
+    }
+
+    private bool CanMove(int x, int y)
+    {
+        char tile = map.map[y, x];
+        return tile != '#' && tile != '|' && tile != '-';
+    }
+
+    public void Attack(Player player)
+    {
+        player.TakeDamage(1);
+    }
+
+    public void TakeDamage(int amount)
+    {
+        healthSystem.TakeDamage(amount);
+    }
+
+    public int Health => healthSystem.Health;
+}
---
+++ Text_Based_RPG _First_Playable/Player.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Player
+{
+    public (int x, int y) Position { get; private set; }
+    private HealthSystem healthSystem;
+    private Map map;
+
+    public Player(Map map, int initialHealth, int startX, int startY)
+    {
+        this.map = map;
+        Position = (startX, startY);
+        healthSystem = new HealthSystem(initialHealth);
+    }
+
+    public void Move(int deltaX, int deltaY)
+    {
+        int newX = Position.x + deltaX;
+        int newY = Position.y + deltaY;
+
+        if (map.WithinBounds(newX, newY) && CanMove(newX, newY))
+        {
+            Position = (newX, newY);
+        }
+    }
+
+    private bool CanMove(int x, int y)
+    {
+        char tile = map.map[y, x];
+        return tile != '#' && tile != '|' && tile != '-';
+    }
+
+    public void Attack(Enemy enemy)
+    {
+        enemy.TakeDamage(1);
+    }
+
+    public void TakeDamage(int amount)
+    {
+        healthSystem.TakeDamage(amount);
+    }
+
+    public int Health => healthSystem.Health;
+}
