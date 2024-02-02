e3b602da4e7a99d7944bce56acabeaa4c379b424
Arman <144450534+JoeMac12@users.noreply.github.com>
2024-02-01 22:24:40 -0400

    create basic health system class

---
+++ Text_Based_RPG _First_Playable/HealthSystem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class HealthSystem
+{
+    public int Health { get; private set; }
+
+    public HealthSystem(int initialHealth)
+    {
+        Health = initialHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        Health = Math.Max(0, Health - damage);
+    }
+}
