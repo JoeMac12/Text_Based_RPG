73244c5a6c0a3dfa49f01ff9920013603e71bb8e
Arman <144450534+JoeMac12@users.noreply.github.com>
2024-02-02 13:56:56 -0400

    remove unused heal + add commits

--- Text_Based_RPG _First_Playable/HealthSystem.cs
+++ Text_Based_RPG _First_Playable/HealthSystem.cs
@@ -6,21 +6,16 @@ using System.Threading.Tasks;
 
 internal class HealthSystem
 {
-    public int Health { get; private set; }
+    public int Health { get; private set; } // Get the health
 
-    public HealthSystem(int initialHealth)
+    public HealthSystem(int initialHealth) // For setting the initial health at the start of the game
     {
         Health = initialHealth;
     }
 
-    public void TakeDamage(int amount)
+    public void TakeDamage(int amount) // Simple take damage method
     {
         Health -= amount;
-        if (Health < 0) Health = 0; // Adding this incase health somehow becomes -1
-    }
-
-    public void Heal(int amount) // Unused atm
-    {
-        Health += amount;
+        if (Health < 0) Health = 0; // Incase health somehow becomes -1
     }
 }
