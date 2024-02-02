faa8e21b516fd00d6581f8bfb4436676798f4309
Arman <144450534+JoeMac12@users.noreply.github.com>
2024-02-01 20:31:11 -0400

    Create and add entity base class

---
+++ Text_Based_RPG _First_Playable/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal abstract class Entity // Base class for initializing the player and enemy
+{
+    public (int x, int y) Position { get; protected set; }
+    public int Health { get; protected set; }
+    protected Map map;
+
+    protected Entity(Map gameMap)
+    {
+        map = gameMap;
+    }
+
+    public abstract void Move();
+}
