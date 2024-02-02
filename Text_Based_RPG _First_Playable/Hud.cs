cc14d155582ce9a9fed2f83c530af5170921c458
Arman <144450534+JoeMac12@users.noreply.github.com>
2024-02-02 01:20:50 -0400

    add and update player hud

---
+++ Text_Based_RPG _First_Playable/Hud.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class HUD
+{
+    private Player player;
+    private int goldScore;
+    private string actionMessage;
+
+    public HUD(Player player)
+    {
+        this.player = player;
+        this.goldScore = 0;
+        this.actionMessage = "";
+    }
+
+    public void UpdateGoldScore(int score)
+    {
+        goldScore += score;
+    }
+
+    public void SetActionMessage(string message)
+    {
+        actionMessage = message;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"Player Health: {player.Health}");
+        Console.ResetColor();
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine($"Gold: {goldScore} / 10");
+        Console.ResetColor();
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine($"Action: {actionMessage}");
+        Console.ResetColor();
+    }
+}
