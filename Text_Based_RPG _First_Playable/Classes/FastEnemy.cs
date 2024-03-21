52dac496423f35c59016ade75467768cc80c46c9
Arman <144450534+JoeMac12@users.noreply.github.com>
2024-03-21 00:52:24 -0300

    clean up this

--- Text_Based_RPG _First_Playable/Classes/FastEnemy.cs
+++ Text_Based_RPG _First_Playable/Classes/FastEnemy.cs
@@ -9,17 +9,17 @@ namespace Text_Based_RPG__First_Playable.Classes
 {
     internal class FastEnemy : Enemy
     {
-        public FastEnemy(Map map, int initialHealth, int startX, int startY, int damage) // Initialize fast enemy
-            : base(map, initialHealth, startX, startY, damage)
+        public FastEnemy(Map map, int initialHealth, int startX, int startY, int damage) : base(map, initialHealth, startX, startY, damage) // Initialize fast enemy
         {
         }
 
         public override void MoveRandomly(Player player, HUD hud) // Use base enemy movement
         {
-            for (int i = 0; i < 2; i++) // Move twice in one turn
+            for (int moveCount = 0; moveCount < 2; moveCount++)
             {
                 base.MoveRandomly(player, hud);
-                if (Position.x == player.Position.x && Position.y == player.Position.y) // Attack Player
+
+                if (Position == player.Position) // Attack Player
                 {
                     Attack(player, hud);
                     hud.SetActionMessage($"You took {damage} damage from fast enemy");
